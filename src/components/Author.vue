4a1553e542bb154598bbed7f7add794159b62d86
Ayyub <mdayyub@users.noreply.github.com>
2020-09-25 12:57:09 +0530

    Update Author.vue
    updated author intro

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -8,7 +8,7 @@
 		</h1>
 
 		<p class="author__intro">
-			A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.
+			A simple Blog
 		</p>
 
 		<p class="author__links">
